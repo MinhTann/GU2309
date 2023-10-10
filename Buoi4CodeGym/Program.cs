098996da2cd66eaceda693e6a62a211cdd919838
MinhTann <tanminhhuynh7@gmail.com>
2023-10-10 18:17:41 +0700

    buoi4

---
+++ Buoi4CodeGym/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+namespace MinhTan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3, b = 2;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Good Evening");
+            Console.WriteLine("Huỳnh Minh Tân");
+            Console.WriteLine(a++ + ++b);
+        }
+    }
+}
