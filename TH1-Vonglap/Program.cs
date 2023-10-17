b885a30067f2ef226e2fe671c983fcc1ddcb988a
MinhTann <tanminhhuynh7@gmail.com>
2023-10-17 20:53:04 +0700

    th2-vonglap

--- TH1-Vonglap/Program.cs
+++ TH1-Vonglap/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Linear Equation Resolver");
+Console.WriteLine("Given a equation as 'a * x + b = 0', please enter constants:");
+Console.Write("a: ");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.Write("b: ");
+double b = Convert.ToDouble(Console.ReadLine());
+if( a!= 0)
+{
+    double x = -b / a;
+    Console.Write("The Soliution is:" + x);
+}
+else
+{
+    if(b == 0)
+    {
+        Console.Write("Solution is all x");
+
+    } else
+    {
+        Console.Write("Vo nghiem");
+    }
+}
