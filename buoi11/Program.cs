7d33655284bd02dac720dca281a5e9bfb7c34135
MinhTann <tanminhhuynh7@gmail.com>
2023-11-04 16:24:03 +0700

    w05

---
+++ buoi11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+
+List<int> a = new List<int>();
+int n = 10;
+Random rd = new Random();
+for (int i = 0; i < n; i++)
+{
+    a.Add(rd.Next(1, 50));
+}
+Console.WriteLine(String.Join(" ", a));
+
+foreach (int item in a)
+{
+    if (item % 2 == 0)
+    {
+        Console.Write(item + " ");
+    }
+    
+    
+}
+Console.WriteLine(String.Join(" ", a.Where(item => item % 2 == 0)));
+
+Console.WriteLine(String.Join(" ", a.Where(item => isChinhPhuong(item))));
+Console.WriteLine(String.Join(" ", a.Where(item => isSole(item)
+
+static bool isChinhPhuong(int n)
+{
+    int i = (int)Math.Sqrt(n);
+    return i * i == n;
+}
+static bool isSole(int n)
+{
+    if(n % 2 != 0)
+    {
+        return n * 2 = n;
+       
+    }
+    return false;
+}
+
