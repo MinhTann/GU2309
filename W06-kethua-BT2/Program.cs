924910bcc86c708963e9b2e90e9cd6d6f84a1c2c
MinhTann <tanminhhuynh7@gmail.com>
2023-11-20 12:11:12 +0700

    a

---
+++ W06-kethua-BT2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace W06_kethua_BT2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point2D point2D = new Point2D(2, 3.2);
+            Console.WriteLine(point2D.ToString());
+
+            Point3D point3D = new Point3D(2, 3.2, 3);
+            Console.WriteLine(point3D.ToString());
+            Console.WriteLine(string.Join(" ", point3D.GetXYZ()));
+
+            Console.ReadKey();
+        }
+    }
+}
